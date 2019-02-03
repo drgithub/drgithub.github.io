0cb7e12f9e0fd82882704360b3f17afd7bc9cdae
Dino Rico Bendanillo <drbendanillo@gmail.com>
2019-02-03 23:23:39 +0800

    Updates

---
+++ precache-manifest.599f478053d5173e9141302bd66d3124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8d7f264bd6bb0762e3c",
+    "url": "/static/js/main.d8d7f264.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d8d7f264bd6bb0762e3c",
+    "url": "/static/css/main.e04b0d7a.chunk.css"
+  },
+  {
+    "revision": "4385d590df18437c368acc8e4047f24e",
+    "url": "/index.html"
+  }
+];
