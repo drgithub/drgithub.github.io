eba8e6e424428f12ecd38823d5e534a4d66fcaf0
Dino Rico Bendanillo <drbendanillo@gmail.com>
2019-02-04 03:50:37 +0800

    Updates

--- precache-manifest.a814f4a65ab6ae23ce40c5df19174de4.js
+++ precache-manifest.a814f4a65ab6ae23ce40c5df19174de4.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "cebd0b5cc2941592a590",
-    "url": "/portfolio/static/js/main.cebd0b5c.chunk.js"
+    "revision": "9018f0d990c4c68caeea",
+    "url": "/portfolio/static/js/main.9018f0d9.chunk.js"
   },
   {
     "revision": "79f959ab817480af6064",
     "url": "/portfolio/static/js/1.79f959ab.chunk.js"
   },
   {
-    "revision": "bd778787d79412a6af335c8d6219f8b4",
+    "revision": "25d035955470a8df7c22d0c48a60862c",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4e700d604764f068a5294de8fb9b58fe.js"
+  "/portfolio/precache-manifest.a814f4a65ab6ae23ce40c5df19174de4.js"
 );
 
 workbox.clientsClaim();
