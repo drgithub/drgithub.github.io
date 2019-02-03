50ec8c049aad3bf22da3c16b1778e401d5940206
Dino Rico Bendanillo <drbendanillo@gmail.com>
2019-02-04 00:38:23 +0800

    Add  node-modules for router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={"/:lng(en|lt)?"} component={Sample} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
 
 export default App;
+
+class Sample extends Component{
+  render(){
+    return (
+      <div>OH DUDE</div>
+    );
+  }
+}
