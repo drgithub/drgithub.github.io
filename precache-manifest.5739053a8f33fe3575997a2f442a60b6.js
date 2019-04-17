04c8f3883b1332e8350151d97525b89f9b74d2ad
drgithub <drbendanillo@gmail.com>
2019-04-18 00:46:49 +0800

    Updates

--- precache-manifest.5739053a8f33fe3575997a2f442a60b6.js
+++ precache-manifest.5739053a8f33fe3575997a2f442a60b6.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
   },
   {
-    "revision": "b1d305dc24258b9f5cbb",
-    "url": "/portfolio/static/js/main.b1d305dc.chunk.js"
+    "revision": "ff1f5300b7c8a285b465",
+    "url": "/portfolio/static/js/main.ff1f5300.chunk.js"
   },
   {
     "revision": "21239217e04e6cb35a9f",
     "url": "/portfolio/static/js/2.21239217.chunk.js"
   },
   {
-    "revision": "a2e7fc1dd239108a5e9e423109b5b0f9",
+    "revision": "f7d7e2559498781e90b3b9404582e373",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.621f977afccbdf280cca4b58ee673d99.js"
+  "/portfolio/precache-manifest.5739053a8f33fe3575997a2f442a60b6.js"
 );
 
 workbox.clientsClaim();
